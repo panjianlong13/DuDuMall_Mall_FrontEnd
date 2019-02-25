6218e083554e1ab2386a34f768ba941a18cc10d4
chensha <chensha@cn.ibm.com>
2019-02-25 15:56:18 +0800

    update structure

--- app-store/src/index.js
+++ app-store/src/index.js
@@ -1,11 +1,60 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Redirect } from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Loadable from 'react-loadable';
 require('./index.scss');
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const loading = function () {
+  return <div id="root"><div className="loader-wrapper"><div className="loader"></div></div></div>;
+}
+
+const HomePage = Loadable({ loader: () => import('./page/homePage'), loading });
+
+class Root extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+    //this.isAuthenrized = !!cookie.load('userid')
+  }
+
+  componentDidMount() {
+  }
+
+  getPage(props, page) {
+    return this.isAuthenrized ?
+      React.createElement(page, props) :
+      <Redirect to={{
+        pathname: "/login",
+        state: Object.assign({ redirectTo: props.location.pathname }, window.history.state)
+      }} />
+  }
+
+  render() {
+
+    return (
+      <div className="cm4e">
+
+        <BrowserRouter>
+          <div>
+            <Route exact path="/" component={HomePage} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export const InitApp = () => {
+  const rootElement = document.getElementById('root');
+
+  if (rootElement) {
+    ReactDOM.render(<Root />, rootElement);
+  }
+
+};
+
+export default InitApp;
+
+InitApp();
---
+++ app-store/src/page/homePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+
+    return (
+      <div>
+      <h1 className="app-root">homePage</h1>
+      </div>
+    );
+  }
+}
