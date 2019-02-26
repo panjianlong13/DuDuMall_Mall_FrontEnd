948f5465c583eb33045cf46981573fa6ee9677cf
panjianlong13 <876060733@qq.com>
2019-02-26 13:24:17 +0800

    update docker.sh

--- app-store/docker.sh
+++ app-store/docker.sh
@@ -1,2 +1,2 @@
-docker build -t dudu-mall-frontend .
-docker run -d --name dudu-mall-frontend -p 3000:3000 dudu-mall-frontend
+docker build -t dudumal-mall-frontend .
+docker run -d --name dudumal-mall-frontend -p 3000:3000 dudumal-mall-frontend
